341181129e8df2938d87ef2990294381ba3c2a65
Maxime Tricoire <max.tricoire@gmail.com>
2022-08-27 17:18:10 +0200

    add a test

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -7,3 +7,13 @@ pub extern "C" fn hello() {
 pub extern "C" fn add(a: u64, b: u64) -> u64 {
     a + b
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn add_works() {
+        assert_eq!(add(40, 2), 42);
+    }
+}
